1e276d11c71f0a43d5bfe9aa6f5da734635ac995
vandana <vandanapremkumar96@gmail.com>
2026-05-04 19:05:49 +0530

    fix db connection

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Vandu@123",  // ← replace this
-  database: "portfolio",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 db.connect((err) => {
   if (err) {
     console.log("Database connection failed:", err);
