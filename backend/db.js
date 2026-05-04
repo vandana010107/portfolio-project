76c2fb4f4b713eb69ca2ca0ff3d5ffe102224e13
vandana <vandanapremkumar96@gmail.com>
2026-05-04 19:12:44 +0530

    fix cloud db connection

--- backend/db.js
+++ backend/db.js
@@ -5,13 +5,15 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false
   }
 });
+
 db.connect((err) => {
   if (err) {
-    console.log("Database connection failed:", err);
+    console.log("Database connection failed ❌", err);
   } else {
     console.log("MySQL Connected ✅");
   }
